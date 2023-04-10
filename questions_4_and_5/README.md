bacf0e2beba565f45ebcb582b88bedba0179f129
Felipe Rechia <feliperechia@gmail.com>
2023-04-09 21:37:29 -0300

    Remove debugs and add README file to questions 4 and 5

---
+++ questions_4_and_5/README.md
@@ -0,0 +1,12 @@
+# Running unit tests
+
+    make clean; make test ; ./test
+
+# Using the program
+
+    make clean ;  make concat_remove_app ; ./concat_remove_app
+
+Then input the following parameters on three different lines, in order:
+1. starting string (s)
+2. target string (t)
+3. maximum operations (k)
--- questions_4_and_5/main.cpp
+++ questions_4_and_5/main.cpp
@@ -14,14 +14,9 @@ int main()
     getline(cin, target_string);
     cin >> max_operations;
 
-    cout << "s: " << starting_string << endl;
-    cout << "t: " << target_string << endl;
-    cout << "k: " << max_operations << endl;
-
     error_code = ValidateInput(&starting_string, &target_string, max_operations);
     if( error_code != NO_ERROR) {
         cout << "no" << endl;
-        cout << "Error validating input data... error code: " << error_code << endl;
         return 1;
     }
 
